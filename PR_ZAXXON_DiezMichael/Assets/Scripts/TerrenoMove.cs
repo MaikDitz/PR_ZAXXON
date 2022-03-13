e3950006364b73e2963e7a38a2013571d7e50701
Maiki <maikditz@gmail.com>
2022-03-13 19:43:06 +0100

    Instanciacion de Terrenos

---
+++ PR_ZAXXON_DiezMichael/Assets/Scripts/TerrenoMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrenoMove : MonoBehaviour
+{
+    InitGame initGame;
+    float speed;
+
+    InstanciadorTerrenos instanciadorTerrenos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initGame = GameObject.Find("InitGame").GetComponent<InitGame>();
+        instanciadorTerrenos = GameObject.Find("InstanciadorTerreno").GetComponent<InstanciadorTerrenos>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = initGame.spaceshipSpeed;
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        float posZ = transform.position.z;
+        if (posZ <= -50)
+        {
+            instanciadorTerrenos.SendMessage("CrearTerreno");
+            Destroy(gameObject);
+        }
+
+    }
+}
